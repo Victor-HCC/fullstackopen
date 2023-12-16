a17c0f8028c1fa273b99916835afca7ab8b2cc44
Victor <vicktorcori@hotmail.com>
2023-12-16 16:57:08 -0300

    Ejercicio 2.13

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -6,6 +6,7 @@ import DetailCountry from './components/DetailCountry'
 function App() {
   const [ countries, setCountries ] = useState([])
   const [ searchData, setSearchData]  = useState([])
+  const [ selectedCountry, setSelectedCountry ] = useState('')
 
   useEffect(() => {
     axios
@@ -16,12 +17,16 @@ function App() {
   const findHandler = (e) => {
     const value = e.target.value.toLowerCase().trim();
     if(value) {
+      setSelectedCountry('')
       const data = countries.filter(country => country.name.common.toLowerCase().includes(value))
       setSearchData(data)
     } else {
       setSearchData([])
     }
-    
+  }
+
+  const buttonHandler = (data) => {
+    setSelectedCountry(data)
   }
 
   return (
@@ -29,10 +34,16 @@ function App() {
       <div>
         find countries <input onChange={findHandler} />
       </div>
-      {searchData.length > 10 
+      {selectedCountry ? <DetailCountry country={selectedCountry} /> : searchData.length > 10 
         ? <div>Too many matches, specify another filter</div> 
         : searchData.length > 1 && searchData.length <= 10 
-          ? searchData.map(elem => <div key={elem.cca3}>{elem.name.common}</div>) 
+          ? searchData.map(elem => {
+              return (
+                <div key={elem.cca3}>
+                  {elem.name.common} <button onClick={() => buttonHandler(elem)}>show</button>
+                </div>
+              )
+            }) 
           : searchData.length === 1 && <DetailCountry country={searchData[0]} />}
     </>
   )
