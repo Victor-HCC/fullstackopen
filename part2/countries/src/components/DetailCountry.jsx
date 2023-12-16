3a27f31b5fc6aa1aec7d0a0bf08b4a38385e2b2e
Victor <vicktorcori@hotmail.com>
2023-12-16 20:11:22 -0300

    Ejercicio 2.14

--- part2/countries/src/components/DetailCountry.jsx
+++ part2/countries/src/components/DetailCountry.jsx
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 const DetailCountry = ({ country }) => {
+  const [ weatherInfo, setWeatherInfo ] = useState(null);
+
+  useEffect(() => {
+    axios.get(`http://api.weatherstack.com/current?access_key=${API_KEY}&query=${country.capital}`)
+      .then(res => {
+        setWeatherInfo(res.data)
+      })
+  }, [country.capital])
+
+  if (!weatherInfo) {
+    return <div>Loading information...</div>;
+  }
+
   return (
     <div>
       <h1>{country.name.common}</h1>
@@ -11,6 +28,11 @@ const DetailCountry = ({ country }) => {
         {Object.entries(country.languages).map(elem => <li key={elem[0]}>{elem[1]}</li>)}
       </ul>
       <img src={country.flags.svg} alt={country.flags.alt} height='150px' />
+
+      <h2>Weather in {country.capital}</h2>
+      <p><b>Temperature:</b> {weatherInfo.current.temperature} Celsius</p>
+      <img src={weatherInfo.current.weather_icons[0]} alt='weather_icon' height='80px' />
+      <p><b>Wind:</b> {weatherInfo.current.wind_speed} km/h, direction {weatherInfo.current.wind_dir}</p>
     </div>
   )
 }
