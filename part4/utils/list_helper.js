c5586f053df20bd41088a7ad66314daed08c4de4
Victor <vicktorcori@hotmail.com>
2024-01-01 17:42:35 -0300

    Ejercicio 4.5

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,8 +6,23 @@ const totalLikes = (blogs) => {
   return blogs.reduce((total, blog) => total + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if(blogs.length === 0) return {}
+
+  return blogs.reduce((max, blog) => {
+    return blog.likes > max.likes
+      ? {
+        title: blog.title,
+        author: blog.author,
+        likes: blog.likes
+      }
+      : max
+  }, blogs[0])
+}
+
 // eslint-disable-next-line no-undef
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
