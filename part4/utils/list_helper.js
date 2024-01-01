9de21a45aab3b04b2c57b6dc8c6d78ea57f4e9c1
Victor <vicktorcori@hotmail.com>
2024-01-01 16:24:17 -0300

    Ejercicio 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,7 +2,12 @@ const dummy = (blogs) => {
   if(Array.isArray(blogs)) return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((total, blog) => total + blog.likes, 0)
+}
+
 // eslint-disable-next-line no-undef
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
